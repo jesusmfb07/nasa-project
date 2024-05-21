12491e0baf55fbfd830bc2bc8e78d958abc952d8
jesus.mendoza <jesus.mendoza@limber.io>
2024-05-21 17:29:20 -0500

    feat: starting application

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { View ,Text,Image, StyleSheet} from 'react-native'
+
+export const Header = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.leftContainer}>
+        <Text style={styles.title}>Explore</Text>
+      </View>
+      <View  style={styles.rightContainer}>
+        <Image source={require('../../assets/nasa.png')} style={styles.image}/>
+      </View>
+  </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection:'row',
+        alignItems: 'center',
+    },
+    leftContainer:{
+        flex:1,
+        alignItems: 'flex-start',
+    },
+    rightContainer:{
+        flex:1,
+        alignItems: 'flex-end', 
+    },
+    title:{
+        fontSize:20,
+        color: '#fff',
+    },
+    image:{
+        width: 70,
+        height:70,
+    }
+
+
+})
+export default Header
