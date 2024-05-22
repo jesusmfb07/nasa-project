6d5037c362e88a6c0c9864a5870608f46991399d
jesus.mendoza <jesus.mendoza@limber.io>
2024-05-22 09:01:22 -0500

    feat: Last Five Days Images

---
+++ src/components/LastFiveDaysImages/LastFiveDaysImages.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View ,Text} from 'react-native'
+
+const LastFiveDaysImages = () => {
+  return (
+    <View>
+        <Text> Last fice days</Text>
+
+
+    </View>
+  )
+}
+
+export default LastFiveDaysImages
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useEffect ,useState} from 'react'
 import { View ,StyleSheet} from 'react-native'
-import Header from '../../components/Header'
+import {format,sub} from 'date-fns'
+
+import Header from '../../components/Header';
+import TodaysImage from '../../components/TodaysImage';
+import fetchApi from '../../utils/fetch';
+import { PostImage } from '../../types';
 
 
 export const Home = () => {
+  const [todaysImage,setTodaysImage] = useState<PostImage>({});
+  const [lastFiveDaysImages,setLastFiveDaysImages] = useState<PostImage[]>([]);
+
+  useEffect(() => {
+    const loadTodaysImage = async () => {
+      try{
+        const todaysImageResponse = await fetchApi();
+        setTodaysImage(todaysImageResponse);
+      }catch (error){
+        console.error(error);
+        setTodaysImage({});
+      }
+    
+    };
+
+    const loadLast5DaysImages =async() =>  {
+      try{
+        const date = new Date();
+        const todaysDate =format(date, 'yyyy-MM-dd');
+        const fiveDaysAgoDate  =format(sub(date,{days: 5}), 'yyyy-MM-dd');  
+
+        const lastFiveDaysImagesDesponse =await fetchApi(
+          `&start_date=${fiveDaysAgoDate}&end_date=${todaysDate}`);
+
+      
+          setLastFiveDaysImages(lastFiveDaysImagesDesponse);
+      }catch(error){
+        console.error(error);
+      }
+    }
+    loadTodaysImage().catch(null);
+    loadLast5DaysImages().catch(null);
+  }, []);
+
+  console.log(lastFiveDaysImages);
+
+
   return (
     <View style={styles.container}>
         <Header />
+        <TodaysImage {...todaysImage}/>
     </View>
   )
 }
